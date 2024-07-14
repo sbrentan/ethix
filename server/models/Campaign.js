f3c145b51d214a41da81053dd4596dfb945aec84
Simone Brentan <simobrentan@gmail.com>
2024-07-14 18:15:44 +0200

    Added campaign controller methods and Token DB entity [Issues #5, #7, #10]

--- server/models/Campaign.js
+++ server/models/Campaign.js
@@ -2,32 +2,48 @@ const mongoose = require("mongoose");
 
 const CampaignSchema = new mongoose.Schema(
 	{
-		title: {
-			type: String,
-			required: true
-		},
+        // Blockchain campaign id. Initially empty, will be filled when the campaign is created on the blockchain. 
+        campaignId: {
+            type: String,
+            required: false
+        },
+        // The target amount of the campaign donation
+        target: {
+            type: Number,
+            required: true
+        },
+        // The amount of money each redeemable token donates. Initially empty, will be filled when the campaign is created on the blockchain.
+        tokenDonation: {
+            type: Number,
+            required: false
+        },
+        // The image url of the campaign
         image: {
             type: String,
             required: false
         },
+        // The title of the campaign (equal to the title stored in the contract)
+		title: {
+			type: String,
+			required: true
+		},
+        // The description of the campaign
         description: {
             type: String,
             required: false
         },
+        // The deadline of the campaign (equal to the deadline stored in the contract)
         deadline: {
             type: Date,
             required: true
         },
-        isLive: {
-            type: Boolean,
-            required: true,
-            default: false
-        },
+        // The donor user id        
         donor: {
             type: mongoose.Schema.Types.ObjectId,
             required: true,
             ref: "Donor",
         },
+        // The receiver user id
         receiver: {
             type: mongoose.Schema.Types.ObjectId,
             required: true,
@@ -40,18 +56,3 @@ const CampaignSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("Campaign", CampaignSchema);
-
-
-
-// struct Campaign {
-//     string title;
-//     string image;
-//     string description;
-//     uint256 deadline; //timestamp format
-//     bool isLive;
-//     address donor;
-//     address receiver;
-//     uint256 balance;
-//     uint256 deposit;
-//     Token[] tokens;
-// }
--- server/routes/campaignsRoutes.js
+++ server/routes/campaignsRoutes.js
@@ -5,13 +5,37 @@ const ROLES_LIST = require("../config/roles_list");
 const verifyRoles = require("../middleware/verifyRoles");
 const verifyJWT = require("../middleware/verifyJWT");
 
-// router.use(verifyJWT);
+router.use(verifyJWT);
 
 router
 	.route("")
 	.get(
-		// verifyRoles(ROLES_LIST.admin),
 		campaignsController.getAllCampaigns
+	);
+
+router
+	.route("/:id")
+	.get(
+		campaignsController.getCampaign
 	)
+	.post(
+		verifyRoles([ROLES_LIST.donor]),
+		campaignsController.createNewCampaign
+	)
+	.patch(
+		verifyRoles([ROLES_LIST.donor]),
+		campaignsController.updateCampaign
+	)
+	.delete(
+		verifyRoles([ROLES_LIST.donor]),
+		campaignsController.deleteCampaign
+	);
+
+router
+	.route("/:id/associate")
+	.post(
+		verifyRoles([ROLES_LIST.donor]),
+		campaignsController.associateCampaignToBlockchain
+	);
 
 module.exports = router;
