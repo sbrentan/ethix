0529d4e623872164a46af7800cfe9cd836cb561f
Simone Brentan <simobrentan@gmail.com>
2024-07-15 18:07:23 +0200

    Updated redeem token with check on token hashes

--- server/controllers/tokensController.js
+++ server/controllers/tokensController.js
@@ -1,5 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const { WEB3_MANAGER_ACCOUNT, WEB3_CONTRACT_ADDRESS, WEB3_CONTRACT } = require("../config/web3");
+const Token = require("../models/Token");
+const Campaign = require("../models/Campaign");
+const crypto = require('crypto');
 
 const retrieveBlockchainError = (error) => {
     try{
@@ -27,19 +30,24 @@ const redeemToken = asyncHandler(async (req, res) => {
     // Get the token from the request body
     const tokenId = req.body.tokenId;
     const campaignId = req.body.campaignId;
+
+    // check if the token hash is present in db
+    try{
+        token = await checkTokenHash(campaignId, tokenId);
+    } catch (error) {
+        // console.log('Error redeeming token:', error);
+        res.json({ message: "Error redeeming token: " + error.message });
+        res.status(400);
+        return;
+    }
     
     console.log('Redeeming token:', tokenId);
     console.log('Campaign ID:', campaignId);
     const data = WEB3_CONTRACT.methods.redeemToken(campaignId, tokenId).encodeABI();
 
-    const tx = {
-        from: WEB3_MANAGER_ACCOUNT.address,
-        to: WEB3_CONTRACT_ADDRESS,
-        data: data
-    };
-
     try {
         const receipt = await WEB3_CONTRACT.methods.redeemToken(campaignId, tokenId).send({ from: WEB3_MANAGER_ACCOUNT.address });
+        token.redeemed = true;
 
         console.log('Transaction receipt:', receipt);
         res.json({ message: "Token redeemed" });
@@ -50,6 +58,23 @@ const redeemToken = asyncHandler(async (req, res) => {
         res.status(400);
     }
 });
+
+const checkTokenHash = async (campaignId, tokenId) => {
+    campaign = await Campaign.findOne({ campaignId: campaignId }).exec();
+    if (!campaign)
+        throw new Error("Campaign not found");
+
+    token_hash = crypto.createHash('sha256').update(campaignId + tokenId).digest('hex')
+    const token = await Token.findOne({ _id: campaign._id, hash: token_hash }).exec();
+
+    if (!token)
+        throw new Error("Token not valid");
+    else if (token.redeemed)
+        throw new Error("Token already redeemed");
+
+    return token;
+}
+
 
 module.exports = {
     redeemToken,
