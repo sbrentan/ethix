ebca36247aa6159a25406f47dbebb5779d19f2fd
Matteo <matteo68.mc@gmail.com>
2024-07-15 15:01:38 +0200

    Added .env for contract address

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,7 +1,7 @@
 import Charity from "./Charity.json";
 
 // Contract address retrieved from Ganache after deploying the contract
-export const CHARITY_CONTRACT_ADDRESS = "0xE933f0cd75be176bf8F734Ae608A104b2d7531c2";
+export const CHARITY_CONTRACT_ADDRESS = process.env.REACT_APP_CHARITY_CONTRACT_ADDRESS;
 
 // The contract ABI retrieved from the compiled contract
 export const CHARITY_CONTRACT_ABI = Charity.abi;
