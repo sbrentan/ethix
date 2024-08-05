fcb4ac211002343b11eee4a793bbd0a7c13c9af8
Simone Brentan <simobrentan@gmail.com>
2024-08-05 15:56:40 +0200

    Fixed token hash errors

--- server/controllers/tokensController.js
+++ server/controllers/tokensController.js
@@ -30,13 +30,13 @@ const redeemToken = asyncHandler(async (req, res) => {
     // Get the token from the request body
     const tokenId = req.body.tokenId;
     const campaignId = req.body.campaignId;
+    const campaignAddress = req.body.campaignAddress;
 
     const block = await web3.eth.getBlock('latest');
-    console.log('Current block:', block.timestamp);
 
     // check if the token hash is present in db
     try{
-        token = await checkTokenHash(campaignId, tokenId);
+        token = await checkTokenHash(campaignId, tokenId, campaignAddress);
     } catch (error) {
         // console.log('Error redeeming token:', error);
         res.json({ message: "Error redeeming token: " + error.message });
@@ -61,12 +61,12 @@ const redeemToken = asyncHandler(async (req, res) => {
     }
 });
 
-const checkTokenHash = async (campaignId, tokenId) => {
-    campaign = await Campaign.findOne({ campaignId: campaignId }).exec();
+const checkTokenHash = async (campaignId, tokenId, campaignAddress) => {
+    campaign = await Campaign.findOne({ _id: campaignId, campaignId: campaignAddress }).exec();
     if (!campaign)
         throw new Error("Campaign not found");
 
-    token_hash = crypto.createHash('sha256').update(campaignId + tokenId).digest('hex')
+    token_hash = crypto.createHash('sha256').update(campaignId + tokenId + campaignAddress).digest('hex')
     const token = await Token.findOne({ campaignId: campaign._id, hash: token_hash }).exec();
 
     if (!token)
