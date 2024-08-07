eed240fb8d36389fa810646a3c480093a648fc29
Alex <alex.reich.al@gmail.com>
2024-08-07 23:36:08 +0200

    Fixed Routes not showing any header or footer in various pages

--- client/src/App.js
+++ client/src/App.js
@@ -24,41 +24,41 @@ import RedeemOverview from "./features/campaigns/RedeemOverview";
 import RedeemPage from "./features/campaigns/RedeemPage";
 
 function App() {
-	return (
-		<>
-			<Routes>
-				<Route path="/" element={<MainLayout />}>
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
                     {/* public routes */}
                     <Route index element={
                         <div className="App">
-                        <header className="App-header">
-                            <img src={logo} className="App-logo" alt="logo" width={100} />
-                            <p>
-                                Edit <code>src/App.js</code> and save to reload.
-                            </p>
-                            <a
-                                className="App-link"
-                                href="https://reactjs.org"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                Learn React
-                            </a>
-                        </header>
-                    </div>
+                            <header className="App-header">
+                                <img src={logo} className="App-logo" alt="logo" width={100} />
+                                <p>
+                                    Edit <code>src/App.js</code> and save to reload.
+                                </p>
+                                <a
+                                    className="App-link"
+                                    href="https://reactjs.org"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Learn React
+                                </a>
+                            </header>
+                        </div>
                     } />
                     <Route path="login" element={<Login />} />
-                    <Route path="logout" element={<Logout />}/>
-                    <Route path="register" element={<Register />}/>
-                    <Route path="home" element={<HomePage />}/>
+                    <Route path="logout" element={<Logout />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="home" element={<HomePage />} />
 
-                    </Route><Route path="redeem">
-                        <Route index element={<RedeemOverview />}/>
+                    <Route path="redeem">
+                        <Route index element={<RedeemOverview />} />
                         <Route path=":token" element={<RedeemPage />} />
                     </Route>
 
                     {/* Protected Routes */}
-					<Route element={<PersistLogin />}>
+                    <Route element={<PersistLogin />}>
 
                         <Route path="campaigns">
                             <Route index element={<CampaignsGrid />} />
@@ -67,112 +67,113 @@ function App() {
                             </Route>
                         </Route>
 
-                    {/* Protected Routes */}
-					<Route element={<PersistLogin />}>
+                        {/* Protected Routes */}
+                        <Route element={<PersistLogin />}>
 
-                        
 
-                        {/* User Routes */}
-                        <Route
-                            element={
-                                <RequireAuth allowedRoles={[ROLES.User]} />
-                            }
-                        >
-                            <Route path="user">
-                                <Route index element={<>User</>} />
-                                <Route path="campaigns">
-                                    <Route index element={<CampaignsGrid />} />
+
+                            {/* User Routes */}
+                            <Route
+                                element={
+                                    <RequireAuth allowedRoles={[ROLES.User]} />
+                                }
+                            >
+                                <Route path="user">
+                                    <Route index element={<>User</>} />
+                                    <Route path="campaigns">
+                                        <Route index element={<CampaignsGrid />} />
+                                    </Route>
                                 </Route>
                             </Route>
-                        </Route>
 
-                        {/* Donor Routes */}
-                        <Route
-                            element={
-                                <RequireAuth allowedRoles={[ROLES.Donor]} />
-                            }
-                        >
-                            <Route path="donor">
-                                <Route index element={<>Donor</>} />
-                                <Route
-                                    path="dashboard"
-                                    element={<p>dashboard</p>}
-                                />
-                                <Route
-                                    path="profile"
-                                    element={<MyProfileRequest />}
-                                />
-                                <Route
-                                    path="donorCampaigns"
-                                    element={<MyCampaigns />}
-                                />
-                                <Route
-                                    path="newCampaign"
-                                    element={<NewCampaign />}
-                                />
-                            </Route>
-                        </Route>
-
-                        {/* Beneficiary Routes */}
-                        <Route
-                            element={
-                                <RequireAuth allowedRoles={[ROLES.Beneficiary]} />
-                            }
-                        >
-                            <Route path="beneficiary">
-                                <Route index element={<>Beneficiary</>} />
-                                <Route
-                                    path="dashboard"
-                                    element={<p>dashboard</p>}
-                                />
-                                <Route
-                                    path="beneficiaryCampaigns"
-                                    element={<MyCampaigns />}
-                                />
-                                <Route
-                                    path="profile"
-                                    element={<MyProfileRequest />}
-                                />
-                            </Route>
-                        </Route>
-
-                        {/* Admin Routes */}
-                        <Route
-                            element={
-                                <RequireAuth allowedRoles={[ROLES.Admin]} />
-                            }
-                        >
-                            <Route path="admin">
-                                <Route index element={<>Admin</>} />
-                                <Route path="users">
-                                    <Route index element={<UsersList />} />
-                                    /* <Route
-                                        path=":id"
-                                        element={<EditUser />}
+                            {/* Donor Routes */}
+                            <Route
+                                element={
+                                    <RequireAuth allowedRoles={[ROLES.Donor]} />
+                                }
+                            >
+                                <Route path="donor">
+                                    <Route index element={<>Donor</>} />
+                                    <Route
+                                        path="dashboard"
+                                        element={<p>dashboard</p>}
                                     />
                                     <Route
-                                        path="new"
-                                        element={<NewUserForm />}
-                                    /> 
-                                </Route>
-                                <Route path="campaigns">
-                                    <Route index element={<CampaignsList />} />
-                                </Route>
-                                <Route path="requests">
-                                    <Route index element={<ProfileRequestsList />} />
+                                        path="profile"
+                                        element={<MyProfileRequest />}
+                                    />
+                                    <Route
+                                        path="donorCampaigns"
+                                        element={<MyCampaigns />}
+                                    />
+                                    <Route
+                                        path="newCampaign"
+                                        element={<NewCampaign />}
+                                    />
                                 </Route>
                             </Route>
+
+                            {/* Beneficiary Routes */}
+                            <Route
+                                element={
+                                    <RequireAuth allowedRoles={[ROLES.Beneficiary]} />
+                                }
+                            >
+                                <Route path="beneficiary">
+                                    <Route index element={<>Beneficiary</>} />
+                                    <Route
+                                        path="dashboard"
+                                        element={<p>dashboard</p>}
+                                    />
+                                    <Route
+                                        path="beneficiaryCampaigns"
+                                        element={<MyCampaigns />}
+                                    />
+                                    <Route
+                                        path="profile"
+                                        element={<MyProfileRequest />}
+                                    />
+                                </Route>
+                            </Route>
+
+                            {/* Admin Routes */}
+                            <Route
+                                element={
+                                    <RequireAuth allowedRoles={[ROLES.Admin]} />
+                                }
+                            >
+                                <Route path="admin">
+                                    <Route index element={<>Admin</>} />
+                                    <Route path="users">
+                                        <Route index element={<UsersList />} />
+                                    /* <Route
+                                            path=":id"
+                                            element={<EditUser />}
+                                        />
+                                        <Route
+                                            path="new"
+                                            element={<NewUserForm />}
+                                        />
+                                    </Route>
+                                    <Route path="campaigns">
+                                        <Route index element={<CampaignsList />} />
+                                    </Route>
+                                    <Route path="requests">
+                                        <Route index element={<ProfileRequestsList />} />
+                                    </Route>
+                                </Route>
+                            </Route>
+
                         </Route>
 
+                        {/* 404 Page not Found */}
+                        <Route path="*" element={<NotFoundResult />} />
+
+                        </Route>
                     </Route>
-
-                    {/* 404 Page not Found */}
-					<Route path="*" element={<NotFoundResult />} />
-
-                </Route>
-			</Routes>
-		</>
-	);
+            </Routes>
+        </>
+    );
 }
 
 export default App;
